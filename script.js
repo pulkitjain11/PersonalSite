6d539fac87e3afbdcb5909824f2747390898af1f
Pulkit Jain <pulkitjainjk@gmail.com>
2022-08-17 00:53:06 +0530

    Added Animation, removed carousel, added animation

--- script.js
+++ script.js
@@ -112,3 +112,140 @@ $(document).ready(function () {
      // }
   });
 });
+
+
+// CODEPEN
+
+var canvas = document.querySelector("#scene"),
+  ctx = canvas.getContext("2d"),
+  particles = [],
+  amount = 0,
+  mouse = {x:0,y:0},
+  radius = 1;
+
+var colors = ["#468966","#FFF0A5", "#FFB03B","#B64926", "#8E2800"];
+
+let copy = document.querySelector("#copy");
+
+var ww = canvas.width = window.innerWidth;
+var wh = canvas.height = window.innerHeight;
+
+function Particle(x,y){
+  this.x =  Math.random()*ww;
+  this.y =  Math.random()*wh;
+  this.dest = {
+    x : x,
+    y: y
+  };
+  this.r =  Math.random()*5 + 2;
+  this.vx = (Math.random()-0.5)*20;
+  this.vy = (Math.random()-0.5)*20;
+  this.accX = 0;
+  this.accY = 0;
+  this.friction = Math.random()*0.05 + 0.94;
+
+  this.color = colors[Math.floor(Math.random()*6)];
+}
+
+Particle.prototype.render = function() {
+
+
+  this.accX = (this.dest.x - this.x)/1000;
+  this.accY = (this.dest.y - this.y)/1000;
+  this.vx += this.accX;
+  this.vy += this.accY;
+  this.vx *= this.friction;
+  this.vy *= this.friction;
+
+  this.x += this.vx;
+  this.y +=  this.vy;
+
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+  ctx.fill();
+
+  var a = this.x - mouse.x;
+  var b = this.y - mouse.y;
+
+  var distance = Math.sqrt( a*a + b*b );
+  if(distance<(radius*70)){
+    this.accX = (this.x - mouse.x)/100;
+    this.accY = (this.y - mouse.y)/100;
+    this.vx += this.accX;
+    this.vy += this.accY;
+  }
+
+}
+
+function onMouseMove(e){
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+}
+
+function onTouchMove(e){
+  if(e.touches.length > 0 ){
+    mouse.x = e.touches[0].clientX;
+    mouse.y = e.touches[0].clientY;
+  }
+}
+
+function onTouchEnd(e){
+mouse.x = -9999;
+mouse.y = -9999;
+}
+
+function initScene(){
+  ww = canvas.width = window.innerWidth;
+  wh = canvas.height = window.innerHeight;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.font = "bold "+(ww/10)+"px sans-serif";
+  ctx.textAlign = "center";
+  ctx.fillText(copy.value, ww/2, wh/2);
+
+  var data  = ctx.getImageData(0, 0, ww, wh).data;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.globalCompositeOperation = "screen";
+
+  particles = [];
+  for(var i=0;i<ww;i+=Math.round(ww/150)){
+    for(var j=0;j<wh;j+=Math.round(ww/150)){
+      if(data[ ((i + j*ww)*4) + 3] > 150){
+        particles.push(new Particle(i,j));
+      }
+    }
+  }
+  amount = particles.length;
+
+}
+
+function onMouseClick(){
+  radius++;
+  if(radius ===5){
+    radius = 0;
+  }
+}
+
+function render(a) {
+  requestAnimationFrame(render);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < amount; i++) {
+    particles[i].render();
+  }
+};
+
+copy.addEventListener("keyup", initScene);
+window.addEventListener("resize", initScene);
+window.addEventListener("mousemove", onMouseMove);
+window.addEventListener("touchmove", onTouchMove);
+window.addEventListener("click", onMouseClick);
+window.addEventListener("touchend", onTouchEnd);
+initScene();
+requestAnimationFrame(render);
+
+
+
+
+particlesJS("particles-js", {"particles":{"number":{"value":14,"density":{"enable":true,"value_area":394.57459926017265}},"color":{"value":"#00d6cc"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.18939580764488287,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":6.6,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":299.8766954377312,"color":"#ffffff","opacity":0.2130702836004932,"width":0.5},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
